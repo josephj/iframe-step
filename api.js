719a6bb2322afceab025e9a469a378a329531b00
Joseph Chiang <joseph_chiang@miiicasa.com>
2013-02-21 13:39:38 +0800

    Add API documentation.

---
+++ api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "IframeStep"
+    ],
+    "modules": [
+        "iframe-step"
+    ],
+    "allModules": [
+        {
+            "displayName": "iframe-step",
+            "name": "iframe-step",
+            "description": "A iframe step control provides a widget for browsing among frames horizontally in an overloaded page region."
+        }
+    ]
+} };
+});
